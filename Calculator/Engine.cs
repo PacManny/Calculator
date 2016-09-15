67a47bd4b3835c40c46b7071c749734ad332d0d3
PacManny <eromerobueno@atl.yearup.org>
2016-09-14 23:31:53 -0400

    added calculator

---
+++ Calculator/Engine.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Engine
+    {
+        public List<double> Operands { get; set; }
+        public List<string> Operators { get; set; }
+        public double Total { get; set; }
+
+        MathLayer mathLayer = new MathLayer();
+       
+        public void doCalculation(double FirstSavedOperand, double SecondSavedOperand, string SavedOperator)
+        {
+
+            switch (SavedOperator)
+            {
+                case "+":
+                    doAddition(FirstSavedOperand, SecondSavedOperand);
+                    break;
+                case "-":
+                    doSubtraction(FirstSavedOperand, SecondSavedOperand);
+                    break;
+                case "x":
+                    doMultiplication(FirstSavedOperand, SecondSavedOperand);
+                    break;
+                case "/":
+                    doDivision(FirstSavedOperand, SecondSavedOperand);
+                    break;
+                default:
+                    break;
+
+            }
+
+        }
+        public void doAddition(double FirstNumber, double SecondNumber)
+        {
+            Total = mathLayer.Add(FirstNumber, SecondNumber);
+
+        }
+        public void doSubtraction(double FirstNumber, double SecondNumber)
+        {
+            Total = mathLayer.Subtract(FirstNumber, SecondNumber);
+        }
+        public void doDivision(double FirstNumber, double SecondNumber)
+        {
+            Total = mathLayer.Divide(FirstNumber, SecondNumber);
+        }
+        public void doMultiplication(double FirstNumber, double SecondNumber)
+        {
+            Total = mathLayer.Multiply(FirstNumber, SecondNumber);
+        }
+
+    }
+}
---
+++ Calculator/MainPage.xaml.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Calculator
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            //this.CalculatorView = new CalculatorControlViewModel();
+            CalculatorEngine.Operands = new List<double>();
+            CalculatorEngine.Operators = new List<string>();
+        }
+        //public CalculatorControlViewModel CalculatorView { get; set; }
+        Engine CalculatorEngine = new Engine();
+ 
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string operandString;
+            double operandDouble;
+            int lastIndexedOperand;
+            string lastIndexedOperator;
+            Button ThisButton = (Button)sender;
+            operandString = ThisButton.Content.ToString();
+            try
+            {
+                operandDouble = Convert.ToDouble(operandString);
+                CalculatorEngine.Operands.Add(operandDouble);
+            }
+            catch
+
+
+            lastIndexedOperator = CalculatorEngine.Operators.Add(operandString);
+            if (ContainsOperator(CalculatorEngine.Operators[]))
+                {
+
+                }
+
+            }
+
+
+            lastIndexedOperand = CalculatorEngine.Operands.Count() - 1;
+            Display.Text += CalculatorEngine.Operands[lastIndexedOperand];
+     
+
+            
+    }
+        public void Equals()
+        {
+            Displayed = "";
+            SecondSavedOperand = input;
+            OperationsDisplay.Text = string.Empty;
+            if (CalculatorEngine.flag)
+            {
+                FirstSavedOperand = CalculatorEngine.Total.ToString();
+                CalculatorEngine.doCalculation(FirstSavedOperand, SecondSavedOperand);
+                Display.Text = CalculatorEngine.Total.ToString();
+
+            }
+            else {
+                CalculatorEngine.doCalculation(FirstSavedOperand, SecondSavedOperand);
+                Display.Text = CalculatorEngine.Total.ToString();
+                CalculatorEngine.flag = true;
+            }
+
+        }
+
+        public bool ContainsOperator(string MathOperator)
+        {
+            List<string> validOperators = new List<string> { "+", "-", "/", "*", "=" };
+            return validOperators.Contains(MathOperator);
+
+        }
+
+        //  private void ButtonAdd_Click(object sender, RoutedEventArgs e)
+        //  {
+        //      input = this.Display.Text;
+        //      FirstSavedOperand = input;
+        //      CalculatorEngine.SavedOperator = "+";
+        //      Displayed += input + " + ";
+        //      OperationsDisplay.Text = Displayed;
+        //      input = string.Empty;
+        //  }
+
+        //  private void ButtonSubtract_Click(object sender, RoutedEventArgs e)
+        //  {
+        //      input = this.Display.Text;
+        //      FirstSavedOperand = input;
+        //      CalculatorEngine.SavedOperator = "-";
+        //      Displayed += input + " - ";
+        //      OperationsDisplay.Text = Displayed;
+        //      input = string.Empty;
+        //  }
+
+        //  private void ButtonMultiply_Click(object sender, RoutedEventArgs e)
+        //  {
+        //      input = this.Display.Text;
+        //      FirstSavedOperand = input;
+        //      CalculatorEngine.SavedOperator = "x";
+        //      Displayed += input + " x ";
+        //      OperationsDisplay.Text = Displayed;
+        //      input = string.Empty;
+        //  }
+
+        //  private void ButtonDivide_Click(object sender, RoutedEventArgs e)
+        //  {
+        //      input = this.Display.Text;
+        //      FirstSavedOperand = input;
+        //      CalculatorEngine.SavedOperator = "+";
+        //      Displayed += input + " + ";
+        //      OperationsDisplay.Text = Displayed;
+        //      input = string.Empty;
+        //  }
+
+        //  private void ButtonNegate_Click(object sender, RoutedEventArgs e)
+        //  {
+        //      if (input.Contains("-"))
+        //      {
+        //          input = input.TrimStart('-');
+        //      }
+        //      else {
+        //          FirstSavedOperand = input;
+        //          input = "-" + input;
+        //      }
+        //      Display.Text = input;
+        //  }
+
+        //  private void ButtonBack_Click(object sender, RoutedEventArgs e)
+        //  {
+        //      int index = input.Count();
+        //      try
+        //      {
+        //          input = input.Remove(index - 1, 1);
+        //      }
+        //      catch
+        //      {
+        //          input = "0";
+        //      }
+        //      Display.Text = input;
+        //  }
+
+        //  private void ButtonClear_Click(object sender, RoutedEventArgs e)
+        //  {
+        //      CalculatorEngine.flag = false;
+        //      OperationsDisplay.Text = "";
+        //      input = "";
+        //      Display.Text = "0";
+        //      CalculatorEngine.Total = 0.0;
+        //      CalculatorEngine.SavedOperator = "";
+        //      Displayed = "";
+        //  }
+
+        //  private void ButtonClearEntry_Click(object sender, RoutedEventArgs e)
+        //  {
+        //      input = "";
+        //      Display.Text = "0";
+        //  }
+    }
+}
